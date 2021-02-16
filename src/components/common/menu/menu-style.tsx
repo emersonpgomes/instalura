8f512fc7fbda736f81de490ebb7c98a1f62154d3
Emerson P. Gomes <emersonpgomes@gmail.com>
2021-02-16 00:01:07 -0200

    fix: remove código que estava causando problema no build

--- src/components/common/menu/menu-style.tsx
+++ src/components/common/menu/menu-style.tsx
@@ -1,6 +1,4 @@
-import styled, { css } from 'styled-components';
-// import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia';
-// import { TextStyleVariants } from '../../../foundation/Text';
+import styled from 'styled-components';
 
 export const MenuStyle = styled.nav`
   display: flex;
@@ -10,41 +8,12 @@ export const MenuStyle = styled.nav`
   margin-top: 18px;
   padding-left: 28px;
   padding-right: 28px;
-  /* ${breakpointsMedia({
-    md: css`
-      justify-content: flex-start;
-      margin-top: 32px;
-      margin-left: auto;
-      margin-right: auto;
-      width: 100%;
-      padding: 0 16px;
-      max-width: 768px;
-    `,
-    lg: css`
-      max-width: 1160px;
-    `,
-    xl: css`
-      max-width: 1222px;
-    `,
-  })} */
 `;
 
 export const LeftSideMenuStyle = styled.div`
   padding: 0;
   margin: 0;
   order: 1;
-  /* ${breakpointsMedia({
-    md: css`
-      width: 131px;
-      height: 32px;
-    `,
-  })}
-  ${breakpointsMedia({
-    md: css`
-      order: initial;
-      padding-right: 16px;
-    `,
-  })} */
 `;
 
 export const CentralSideMenuStyle = styled.div`
@@ -61,32 +30,13 @@ export const CentralSideMenuStyle = styled.div`
   border-bottom: 1px solid ${({ theme }) => theme.colors.borders.main.color};
   padding: 12px;
 
-  /* ${breakpointsMedia({
-    md: css`
-      max-width: 332px;
-      justify-content: space-between;
-      flex: 1;
-      order: initial;
-      border: none;
-      margin: 0;
-      padding-top: 0;
-      padding-bottom: 0;
-    `,
-  })} */
   a {
     text-align: center;
     display: block;
     text-decoration: none;
     color: #88989e;
     transition: 200ms ease-in-out;
-    /* ${breakpointsMedia({
-      xs: css`
-        ${TextStyleVariants.smallestException}
-      `,
-      md: css`
-        ${TextStyleVariants.paragraph1}
-      `,
-    })} */
+
     &:hover,
     &:focus {
       font-weight: 500;
@@ -102,9 +52,4 @@ export const RightSideMenuStyle = styled.div`
   flex: 1;
   order: 2;
   justify-content: flex-end;
-  /* ${breakpointsMedia({
-    md: css`
-      order: initial;
-    `,
-  })} */
 `;
